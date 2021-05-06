21e71cf22ef897a35670d524b871967736e0d17c
Miqueias <mi.queias2010@hotmail.com>
2021-05-06 08:28:39 -0300

    [MOCK] iniciando testes no gerador de pagamentos

---
+++ Mockito/java-mock/src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
@@ -0,0 +1,56 @@
+package br.com.alura.leilao.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import br.com.alura.leilao.dao.LeilaoDao;
+import br.com.alura.leilao.dao.PagamentoDao;
+import br.com.alura.leilao.model.Lance;
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Usuario;
+
+class GeradorDePagamentoTest {
+
+	private GeradorDePagamento gerador;
+	
+	@Mock
+	private PagamentoDao pagamentoDao;
+	
+	@BeforeEach
+	public void beforeEach() {
+		MockitoAnnotations.initMocks(this);
+		this.gerador = new GeradorDePagamento(pagamentoDao);
+	}
+	@Test
+	void deveriaCriarPagamentoParaVencedorDoLeilao() {
+		Leilao leilao = leilao();
+		Lance vencedor = leilao.getLanceVencedor();
+		gerador.gerarPagamento(vencedor);
+		
+		Mockito.verify(pagamentoDao).salvar(null);
+	}
+	
+	private Leilao leilao() {
+		
+		Leilao leilao = new Leilao("Celular",
+				new BigDecimal("500"),
+				new Usuario("Fulano"));
+		
+		Lance lance = new Lance(new Usuario("Ciclano"), 
+				new BigDecimal("900"));
+	
+		leilao.propoe(lance);
+		leilao.setLanceVencedor(lance);
+		
+		return leilao;
+	}
+}
